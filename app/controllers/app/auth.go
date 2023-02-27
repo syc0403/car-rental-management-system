d0ba66808fa29d332e70eb761b8a704c52e61362
施宇诚 <652118918@qq.com>
2023-02-27 14:56:06 +0800

    init

---
+++ app/controllers/app/auth.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	"gin-example/app/common/request"
+	"gin-example/app/common/response"
+	"gin-example/app/services"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func Login(c *gin.Context) {
+	var form request.Login
+	if err := c.ShouldBindJSON(&form); err != nil {
+		response.ValidateFail(c, request.GetErrorMsg(form, err))
+		return
+	}
+
+	if err, user := services.UserService.Login(form); err != nil {
+		response.BusinessFail(c, err.Error())
+	} else {
+		tokenData, err, _ := services.JwtService.CreateToken(services.AppGuardName, user)
+		if err != nil {
+			response.BusinessFail(c, err.Error())
+			return
+		}
+		response.Success(c, tokenData)
+	}
+}
+
+func Logout(c *gin.Context) {
+	err := services.JwtService.JoinBlackList(c.Keys["token"].(*jwt.Token))
+	if err != nil {
+		response.BusinessFail(c, "登出失败")
+		return
+	}
+	response.Success(c, nil)
+}
---
+++ app/controllers/app/user.go
@@ -0,0 +1,33 @@
+package app
+
+import (
+	"gin-example/app/common/request"
+	"gin-example/app/common/response"
+	"gin-example/app/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Register 用户注册
+func Register(c *gin.Context) {
+	var form request.Register
+	if err := c.ShouldBindJSON(&form); err != nil {
+		response.ValidateFail(c, request.GetErrorMsg(form, err))
+		return
+	}
+
+	if err, user := services.UserService.Register(form); err != nil {
+		response.BusinessFail(c, err.Error())
+	} else {
+		response.Success(c, user)
+	}
+}
+
+func Info(c *gin.Context) {
+	err, user := services.UserService.GetUserInfo(c.Keys["id"].(string))
+	if err != nil {
+		response.BusinessFail(c, err.Error())
+		return
+	}
+	response.Success(c, user)
+}
