bffb8b0d0577bdc41b45eb2a20b7bee604ff9aee
施宇诚 <652118918@qq.com>
2023-02-27 17:15:07 +0800

    fix: update routes

--- routes/api.go
+++ routes/api.go
@@ -1,43 +1,23 @@
 package routes
 
 import (
-	"car-rental-management-system/app/common/request"
 	"car-rental-management-system/app/controllers/app"
 	"car-rental-management-system/app/middleware"
 	"car-rental-management-system/app/services"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 // SetApiGroupRoutes 定义 api 分组路由
 func SetApiGroupRoutes(router *gin.RouterGroup) {
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-	router.GET("/test", func(c *gin.Context) {
-		c.String(http.StatusOK, "success")
-	})
-	router.POST("/user/register", func(c *gin.Context) {
-		var form request.Register
-		if err := c.ShouldBindJSON(&form); err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"error": request.GetErrorMsg(form, err),
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"message": "success",
-		})
-	})
 
-	router.POST("/auth/register", app.Register)
-	router.POST("/auth/login", app.Login)
+	router.POST("/user/register", app.Register)
+	router.POST("/user/login", app.Login)
 
 	authRouter := router.Group("").Use(middleware.JWTAuth(services.AppGuardName))
 	{
-		authRouter.POST("/auth/info", app.Info)
-		authRouter.POST("/auth/logout", app.Logout)
+		authRouter.POST("/user/info", app.Info)
+		authRouter.POST("/user/logout", app.Logout)
 	}
 
 }
