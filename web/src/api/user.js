a9784adc8f85f97d0ea3ba93e8a9656674ef34f8
施宇诚 <652118918@qq.com>
2023-03-02 17:56:24 +0800

    feat: 基础架构完成

--- web/src/api/user.js
+++ web/src/api/user.js
@@ -16,9 +16,10 @@ export function getInfo(token) {
   })
 }
 
-export function logout() {
+export function logout(token) {
   return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
+    url: '/user/logout',
+    method: 'post',
+    headers: { 'Authorization': 'Bearer ' + token }
   })
 }
