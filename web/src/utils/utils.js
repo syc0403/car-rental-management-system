b31a11be930038564d2cdc5ef37592ea499d1974
施宇诚 <652118918@qq.com>
2023-03-09 15:29:54 +0800

    feat: datetime处理util

--- web/src/utils/utils.js
+++ web/src/utils/utils.js
@@ -135,5 +135,19 @@ function deadline(timeStamp) {
 	}
 
 }
-
-export { formatTime, dateDiff, getTimeStamp, remainTime, deadline }
+function dataTimeFormatter(v){
+	let date = new Date(v);
+	let y = date.getFullYear();
+	let MM = date.getMonth() + 1;
+	MM = MM < 10 ? "0" + MM : MM;
+	let d = date.getDate();
+	d = d < 10 ? "0" + d : d;
+	let h = date.getHours();
+	h = h < 10 ? "0" + h : h;
+	let m = date.getMinutes();
+	m = m < 10 ? "0" + m : m;
+	let s = date.getSeconds();
+	s = s < 10 ? "0" + s : s;
+	return y + "-" + MM + "-" + d + " " + h + ":" + m + ":" + s;
+  }
+export { formatTime, dateDiff, getTimeStamp, remainTime, deadline,dataTimeFormatter }
